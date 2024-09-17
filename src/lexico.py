2c7cf693c85835b1f01ec515bc05372e9af48b15
JosueCR170 <josolorzano4@gmail.com>
2024-09-16 20:41:15 -0600

    analizador lexico bien

--- src/lexico.py
+++ src/lexico.py
@@ -1,6 +1,4 @@
 import re
-#comando para instalar la libreria de analizador lexico pip install ply
-#import ply.lex as lex
 
 keywords = {
     'NUM': r'\bnum\b',               # Declaración de entero
@@ -9,6 +7,8 @@ keywords = {
     'BOOL': r'\bbool\b',             # Declaración de booleano
     'NULO': r'\bnulo\b',             # Declaración de valor nulo
     'CADENA': r'\bcadena\b',         # Declaración de string
+    'Y':r'\b\&{2}\b|\&{2}',               #Operador and
+    'O':r'\b\|{2}\b|\|{2}',               #Operador or
     'SUMA': r'\+',               # Operador suma
     'RESTA': r'\-',              # Operador resta
     'MULT': r'\*',               # Operador multiplicación
@@ -25,7 +25,8 @@ keywords = {
     'ID': r'[a-zA-Z_][a-zA-Z0-9_]*',  # Identificadores
     'NUMERO': r'\d+(\.\d+)?',   # Números (enteros o decimales)
     'TEXTO': r'"[^"]*"',        # Strings entre comillas
-    'CAR': r'\'\w\''
+    'CAR': r'\'\w\'',
+    'ESPACIOS': r'\s'
     }
 
 
@@ -35,33 +36,32 @@ class analisisLexico:
 
     def insert(self, token_type, token_value):
         self.tokens.append((token_type, token_value))
+        
+        
+    def tokenizar(self,tex):
+        tokenRegex='|'.join(f'(?P<{name}>{pattern})' for name, pattern in keywords.items())
+        pos = 0
+        pattern = re.compile(tokenRegex)
+        while pos < len(tex):
+            match = pattern.match(tex,pos)
+            if match:
+                token_type=match.lastgroup
+                token_value = match.group()
+                if token_type != 'ESPACIOS':
+                    self.insert(token_type, token_value)    
+                pos += len(token_value)  
+            else:
+                print(f"(Carácter no reconocido: {tex[pos]})")
+                pos += 1 
+        for token in self.tokens:
+            print(token)
 
-
-def tokenizar(tex):
-    lexer=analisisLexico()
-    
-    tokenRegex='|'.join(f'(?P<{name}>{pattern})' for name, pattern in keywords.items())
-    pos = 0
-    pattern = re.compile(tokenRegex)
-    while pos < len(tex):
-        match = pattern.match(tex,pos)
-        if match:
-            token_type=match.lastgroup
-            token_value = match.group()
-            #print(f"{token_type}, {token_value})")
-            lexer.insert(token_type,token_value)
-            pos += len(token_value)  # Avanzar la posición en el texto
-        else:
-            #print(f"(Carácter no reconocido: {tex[pos]})")
-            pos += 1  # Avanzar manualmente si no se reconoce el carácter
-    for token in lexer.tokens:
-        print(token)
-
-prueba= """num numeros = 33:
+prueba= """num numeros = 90:
 cadena texto es = "texto":
 """
 #print(splitFinal(prueba))
-tokenizar(prueba)
+
+analisisLexico().tokenizar(prueba)
 
 
 

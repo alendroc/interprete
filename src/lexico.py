e6de0c3be9bbe6acf357f3d5a0d965bbde95a5a2
Alejandro <alejandro6chaves@gmail.com>
2024-09-20 17:56:08 -0600

    asignar nuevos tokens

--- src/lexico.py
+++ src/lexico.py
@@ -7,8 +7,8 @@ keywords = {
     'BOOL': r'\bbool\b',             # Declaración de booleano
     'NULO': r'\bnulo\b',             # Declaración de valor nulo
     'CADENA': r'\bcadena\b',         # Declaración de string
-    'Y':r'\b\&{2}\b|\&{2}',               #Operador and
-    'O':r'\b\|{2}\b|\|{2}',               #Operador or
+    'Y':r'\b\&{2}\b|\&{2}',          #Operador and
+    'O':r'\b\|{2}\b|\|{2}',          #Operador or
     'SUMA': r'\+',               # Operador suma
     'RESTA': r'\-',              # Operador resta
     'MULT': r'\*',               # Operador multiplicación
@@ -19,14 +19,26 @@ keywords = {
     'MAYOR_IGUAL': r'>=',        # Operador mayor o igual que
     'ASIGNAR': r'=',             # Asignación de valor
     'DIFERENTE': r'<>',          # Diferente
-    'IGUALDAD': r'\bes\b',          # Comparación de igualdad
-    'NEGACION': r'\bno\b',          # Negación (No igual a)
-    'FIN_LINEA': r':',          # Fin de línea
-    'ID': r'[a-zA-Z_][a-zA-Z0-9_]*',  # Identificadores
+    'PARENTESIS_I': r'\(',       # Parentesis izquierdo
+    'PARENTESIS_D': r'\)',       # Parentesis derecho
+    'CORCHETE_I': r'\[',         # Corchete izquierdo
+    'CORCHETE_D': r'\]',         # Corchete derecho
+    'LLAVE_I': r'\{',            # Llave izquierdo
+    'LLAVE_D': r'\}',            # Llave derecha
+    'IGUALDAD': r'\bes\b',       # Comparación de igualdad
+    'NEGACION': r'\bno\b',       # Negación (No igual a)
+    'FIN_LINEA': r':',           # Fin de línea
     'NUMERO': r'\d+(\.\d+)?',   # Números (enteros o decimales)
+    'VERDADERO': r'\bverdadero\b',
+    'FALSO': r'\bfalso\b',
     'TEXTO': r'"[^"]*"',        # Strings entre comillas
-    'CAR': r'\'\w\'',
-    'ESPACIOS': r'\s'
+    'CAR': r'\'\w\'',           # Caracter
+    'SI': r'\bsi\b',            # si simula el if
+    'SINO': r'\bsino\b',          # sino simula el else
+    'ESPACIOS': r'\s',
+    'SALTO_LINEA': r'\t',           #salto de linea
+    'ID': r'[a-zA-Z_][a-zA-Z0-9_]*',  # Identificadores
+
     }
 
 
@@ -57,7 +69,9 @@ class analisisLexico:
             print(token)
 
 prueba= """num numeros = 90:
-cadena texto es = "texto":
+si
+cadena texto = "texto":
+sino
 """
 #print(splitFinal(prueba))
 

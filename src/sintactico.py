dd0d033b6d2c475e22e5d6641f2b3bfd60760e95
JosueCR170 <josolorzano4@gmail.com>
2024-09-22 17:34:35 -0600

    para el merge

--- src/sintactico.py
+++ src/sintactico.py
@@ -206,7 +206,7 @@ class AnalisisSintactico:
 #FIN DECLARACIONES DE VARIABLES
 
 prueba = """num n = (90 + 20 - 10) / 2 * 2:
-num n2 = n + e:
+num n2 = n + n*2:
 sim n3 = 'm':
 """
 # $IMPRIMIR (n2):

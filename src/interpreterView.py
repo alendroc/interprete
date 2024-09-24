4e22b9feba654fa326693882ed5bffc767a9a8d9
Daniel <danielgonzalezpicad@gmail.com>
2024-09-24 02:28:01 -0600

    Navbar

--- src/interpreterView.py
+++ src/interpreterView.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from .interprete import compilar
+from tkinter import ttk, filedialog
 
 class InterpreterView:
     def __init__(self):
@@ -18,9 +18,7 @@ class InterpreterView:
         self.consoleFr = Frame(self.mainFr, bg="#f0f0f0")
 
         # Text y Scrollbar del código y los números de línea
-        #Cambiar color 
         self.line_number = Text(self.codeFr, width=4, padx=4, takefocus=0, border=0, background='#527E60', state=DISABLED)
-        #Aqui se enlazan los dos, donde estan los numeros y el texto 
         self.codeTxt = Text(self.codeFr, wrap='none')  
         self.scrollbar = Scrollbar(self.codeFr, command=self.on_scroll)
         self.codeTxt.config(yscrollcommand=self.scrollbar.set)
@@ -67,7 +65,7 @@ class InterpreterView:
         self.on_scroll('scroll', event.delta, 'units')
 
     def onEjecEvent(self):
-        print("Hola")
+        print("Ejecutar")
 
     def onCompileEvent(self):
         try:
@@ -88,9 +86,8 @@ class InterpreterView:
         self.optionsFr.pack(fill=X)
         self.debuggerFr.pack(fill=X)  
 
-        # Añadir menuFr al mainFr
-        self.menuFr.pack_propagate(False)
-        self.menuFr.config(height=60)
+        # Crear el navbar
+        self.create_navbar()
 
         # Añadir componentes
         self.line_number.grid(column=0, row=0, padx=10, pady=10, sticky='ns')
@@ -104,6 +101,49 @@ class InterpreterView:
         self.ejecBtn.pack(side=RIGHT, anchor="e", padx=[5, 5])
         self.compBtn.pack(side=RIGHT, anchor="e", padx=[5, 5])
 
+    #AQUI INICIA EL NAVBAR
+    def create_navbar(self):
+        # Crear barra de menú
+        self.menu_bar = Menu(self.mainWd)
+        
+        # Crear menú Opciones, Nosostros y Vista
+        self.options_menu = Menu(self.menu_bar, tearoff=0)
+        self.vista_menu = Menu(self.menu_bar, tearoff=0)
+        self.nosotros_menu = Menu(self.menu_bar, tearoff=0)
+        # Poner todas las opciones que tendrá
+        self.options_menu.add_command(label="Guardar")
+        self.options_menu.add_command(label="Guardar como...")
+        self.vista_menu.add_command(label="Cambiar Tema", command=self.cambiarTema)
+       
+        # Añadir el menú "Opciones" a la barra de menú
+        self.menu_bar.add_cascade(label="Opciones", menu=self.options_menu)
+        self.menu_bar.add_cascade(label="Vista", menu=self.vista_menu)
+        self.menu_bar.add_cascade(label="Sobre Nosotros")
+        # Configurar la barra de menú en la ventana principal
+        self.mainWd.config(menu=self.menu_bar)
+
+    def cambiarTema(self):
+        # Simplemente va cambiar al contrario
+        current_bg = self.codeFr.cget("bg")
+        if current_bg == "#f0f0f0":
+            self.codeFr.config(bg="#2e2e2e")
+            self.consoleFr.config(bg="#2e2e2e")
+            self.codeTxt.config(bg="#1e1e1e", fg="white")
+            self.consoleTxt.config(bg="#1e1e1e", fg="white")
+        else:
+            self.codeFr.config(bg="#f0f0f0")
+            self.consoleFr.config(bg="#f0f0f0")
+            self.codeTxt.config(bg="white", fg="black")
+            self.consoleTxt.config(bg="white", fg="black")
+
+    
+    #Funciones de guardar 
+    #def guardar(self):
+        
+    
+    #def guardarComo(self):
+      
+
     def resize(self, event):
         width = self.mainFr.winfo_width()
         height = self.mainFr.winfo_height()
@@ -115,11 +155,9 @@ class InterpreterView:
         self.consoleFr.place(x=0, y=self.menuFr.winfo_height() + remaining_height * 0.5, width=width, height=remaining_height * 0.5)
 
         # Ajustar tamaños
-        #Es un despiche xd
-        self.codeFr.grid_propagate(False)  
-        self.consoleFr.grid_propagate(False)  
+        self.codeFr.grid_propagate(False)
+        self.consoleFr.grid_propagate(False)
 
         self.codeFr.columnconfigure(1, weight=1)  # Ajuste para que el área de texto sea responsive
         self.consoleFr.columnconfigure(0, weight=1)  
         self.consoleFr.rowconfigure(0, weight=1)
-

8c3c1251856f85dd98c38abf7f42b1320dcc757a
JosueCR170 <josolorzano4@gmail.com>
2024-09-24 23:51:54 -0600

    funciones incorporadas

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-# from src.sintactico import AnalisisSintactico as sintac
-# from src.lexico import AnalisisLexico as lex
 from src.interpreterView import InterpreterView
 
-
-
 def main():
-
-#     prueba = """num n = (90 + 20 - 10) / 2 * 2:
-# num n2 = n + n*2:
-# sim n3 = 'm':
-# $IMPRIMIR (n2):
-# """
     vista=InterpreterView()
-    #codigo=compilar(vista.codeTxt.get())
     vista.onCompileEvent()
-    
-    
-
 if __name__ == "__main__":
     main()
 
